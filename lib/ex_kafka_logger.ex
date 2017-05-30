ba1da3cfeac9e74b49c37cb69b1f7e8a1d1cd40d
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-05-30 13:39:57 -0300

    Initial commit

---
+++ lib/ex_kafka_logger.ex
@@ -0,0 +1,18 @@
+defmodule ExKafkaLogger do
+  @moduledoc """
+  Documentation for ExKafkaLogger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExKafkaLogger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_kafka_logger_test.exs
@@ -0,0 +1,8 @@
+defmodule ExKafkaLoggerTest do
+  use ExUnit.Case
+  doctest ExKafkaLogger
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
