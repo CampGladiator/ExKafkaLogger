15684399db80c1a143e01220cf7f7e183a9213e0
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-06-01 17:19:47 -0300

    Add package descriptions

--- mix.exs
+++ mix.exs
@@ -2,12 +2,17 @@ defmodule ExKafkaLogger.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_kafka_logger,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [ app: :ex_kafka_logger,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "ExKafkaLogger",
+      source_url: "https://github.com/jeffhsta/ExKafkaLogger"
+    ]
   end
 
   def application do
@@ -20,6 +25,20 @@ defmodule ExKafkaLogger.Mixfile do
       {:plug, "~> 1.3"},
       {:kafka_ex, "~> 0.6.5"},
       {:poison, "~> 3.1"},
+    ]
+  end
+
+  defp description do
+    """
+    Elixir logger that serializes to JSON and publish it to Apache Kafka
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Jefferson Stachelski", "Gabriel Alves"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jeffhsta/ExKafkaLogger"}
     ]
   end
 end
