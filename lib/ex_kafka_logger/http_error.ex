d36ce0d73fb7c1a9ec719b428c6c2c89cc94a044
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-06-01 16:44:58 -0300

    Fix typo

--- lib/ex_kafka_logger/http_error.ex
+++ lib/ex_kafka_logger/http_error.ex
@@ -1,22 +1,22 @@
 defmodule ExKafkaLogger.HttpError do
   def template(401) do
-    response_template("Unathorized")
+    meta_template("Unathorized")
   end
 
   def template(404) do
-    response_template("Not found")
+    meta_template("Not found")
   end
 
   def template(422) do
-    response_template("Unprocessable entity")
+    meta_template("Unprocessable entity")
   end
 
   def template(500) do
-    response_template("Internal server error")
+    meta_template("Internal server error")
   end
 
   def template(_) do
-    response_template("unknown error")
+    meta_template("unknown error")
   end
 
   defp meta_template(message) do
