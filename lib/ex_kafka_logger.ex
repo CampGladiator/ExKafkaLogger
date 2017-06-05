66cb4defb6b766c6201a4bd095bbbb48e68a0911
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-06-05 16:21:05 -0300

    Add documentation

--- lib/ex_kafka_logger.ex
+++ lib/ex_kafka_logger.ex
@@ -1,9 +1,50 @@
 defmodule ExKafkaLogger do
+  @moduledoc File.read!(Path.expand("../README.md", __DIR__))
+
   use ExKafkaLogger.API
   use ExKafkaLogger.EventListener
 
+  @doc """
+  Alias to `ExKafkaLogger.log(:info, log)`
+  The argument could be a `String` or a `Map`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.info("Some info to log on Kafka")
+  :ok
+  ```
+  """
   def info(log), do: log(:info, log)
+
+  @doc """
+  Alias to `ExKafkaLogger.log(:error, log)`
+  The argument could be a `String` or a `Map`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.error("Some error to log on Kafka")
+  :ok
+  ```
+  """
   def error(log), do: log(:error, log)
+
+  @doc """
+  Alias to `ExKafkaLogger.log(:debug, log)`
+  The argument could be a `String` or a `Map`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.debug("Something to debug and log on Kafka")
+  :ok
+  ```
+  """
   def debug(log), do: log(:debug, log)
+
+  @doc """
+  Alias to `ExKafkaLogger.log(:warn, log)`
+  The argument could be a `String` or a `Map`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.warn("Some warn to log on Kafka")
+  :ok
+  ```
+  """
   def warn(log), do: log(:warn, log)
 end
--- lib/ex_kafka_logger/api.ex
+++ lib/ex_kafka_logger/api.ex
@@ -4,6 +4,16 @@ defmodule ExKafkaLogger.API do
       @service_name Application.get_env(:ex_kafka_logger, :service_name)
       @default_tracker_id "No-Tracker-ID"
 
+      @doc """
+      The function receive two arguments the log level that is an `Atom` and
+      could be (`:info`, `:debug`, `:warn` or `:error`), the second argument is
+      the log that is a `Map`
+      ## Example
+      ```elixir
+      iex> ExKafkaLogger.log(:info, %{timestamp: "2017-06-05T16:13:30.000Z", log: "Some info to log on Kafka"}, metadata: %{})
+      :ok
+      ```
+      """
       def log(level, content = %{timestamp: timestamp_str}) do
         log = %{
           timestamp: timestamp_str,
@@ -17,11 +27,34 @@ defmodule ExKafkaLogger.API do
         ExKafkaLogger.KafkaClient.produce(log)
       end
 
+      @doc """
+      The function receive two arguments the log level that is an `Atom` and
+      could be (`:info`, `:debug`, `:warn` or `:error`), the second argument is
+      the log that is a `Map`
+
+      It's an alias to `ExKafkaLogger.API.log/2` that receives a log as a `Map`
+      with a `timestamp`
+      ## Example
+      ```elixir
+      iex> ExKafkaLogger.log(:info, %{track_id: 123, log: "Some info to log on Kafka"}, metadata: %{})
+      :ok
+      ```
+      """
       def log(level, content) when is_map(content) do
         new_content = Map.merge(content, %{ timestamp: get_timestamp_str() })
         log(level, new_content)
       end
 
+      @doc """
+      The function receive two arguments the log level that is an `Atom` and
+      could be (`:info`, `:debug`, `:warn` or `:error`), the second argument is
+      the log that is a `String`
+      ## Example
+      ```elixir
+      iex> ExKafkaLogger.log(:info, "Some info to log on Kafka")
+      :ok
+      ```
+      """
       def log(level, content) when is_bitstring(content) do
         log(level, %{log: content})
       end
