6dcf5f116e02d2a309dbfdc2bc1d93c3169fb941
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-09-22 15:46:06 -0300

    Refactors and handling logs as maps

--- lib/ex_kafka_logger/event_listener.ex
+++ lib/ex_kafka_logger/event_listener.ex
@@ -2,39 +2,50 @@ defmodule ExKafkaLogger.EventListener do
   defmacro __using__(_) do
     quote do
       use GenEvent
+      alias ExKafkaLogger.ParseHandler
 
       def init(state), do: {:ok, state}
 
-      def handle_event(data = {level, _pid, {_logger, log_msg, _timestamp, metadata}}, state) do
-        metadata
-        |> Keyword.get(:application)
-        |> case do
-          :kaffe -> {:ok, state}
-          _ ->
-            try do
-              metadata_map = Map.new(metadata)
-
-              content = %{
-                level: level,
-                metadata: Map.delete(metadata_map, :pid),
-                content: convert_message(log_msg),
-                type: convert_log_type(metadata_map.module)
-              }
-
-              log(level, content)
-              {:ok, state}
-            rescue
-              _ -> {:ok, state}
-            end
+      def handle_event({level, _pid, {_logger, log_msg, _timestamp, metadata}}, state) do
+        try do
+          metadata
+          |> Map.new()
+          |> Map.delete(:pid)
+          |> process_log(log_msg, level)
+          {:ok, state}
+        rescue
+          _ -> {:ok, state}
         end
       end
 
-      def handle_call(_, state), do: {:ok, :ok, state}
+      def handle_call(_, state), do: {:ok, state}
+
+      defp process_log(%{application: :kaffe}, _log_msg, _level), do: :ok
+      defp process_log(metadata, log_msg, level) do
+        content = %{
+          level: level,
+          metadata: metadata,
+          content: convert_message(log_msg),
+          type: convert_log_type(metadata.module)
+        }
+
+        ParseHandler.log(level, content)
+      end
 
       defp convert_message(msg) when is_list(msg) do
-        msg |> List.flatten |> Enum.map(&convert_int_asc_to_string/1) |> Enum.join("")
+        msg
+        |> List.flatten
+        |> Enum.map(&convert_int_asc_to_string/1)
+        |> Enum.join("")
       end
-      defp convert_message(msg), do: msg
+      defp convert_message(msg) do
+        msg
+        |> Poison.decode()
+        |> case do
+          {:ok, decoded} -> decoded
+          _ -> msg
+        end
+      end
 
       defp convert_int_asc_to_string(data) when is_bitstring(data), do: data
       defp convert_int_asc_to_string(data) when is_integer(data), do: <<data>>
---
+++ lib/ex_kafka_logger/parse_handler.ex
@@ -0,0 +1,61 @@
+defmodule ExKafkaLogger.ParseHandler do
+  @service_name Application.get_env(:ex_kafka_logger, :service_name)
+
+  alias ExKafkaLogger.KafkaClient
+
+  @doc """
+  The function receive two arguments the log level that is an `Atom` and
+  could be (`:info`, `:debug`, `:warn` or `:error`), the second argument is
+  the log that is a `Map`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.log(:info, %{timestamp: "2017-06-05T16:13:30.000Z", log: "Some info to log on Kafka"}, metadata: %{})
+  :ok
+  ```
+  """
+  def log(level, data = %{timestamp: timestamp, content: content}) do
+    %{
+      timestamp: timestamp,
+      metadata: data |> Map.get(:metadata, %{}),
+      level: level |> Atom.to_string |> String.upcase,
+      service: @service_name,
+      request_id: data |> Map.get(:request_id),
+      content: content
+    }
+    |> Poison.encode!
+    |> KafkaClient.produce()
+  end
+
+  @doc """
+  The function receive two arguments the log level that is an `Atom` and
+  could be (`:info`, `:debug`, `:warn` or `:error`), the second argument is
+  the log that is a `Map`
+
+  It's an alias to `ExKafkaLogger.API.log/2` that receives a log as a `Map`
+  with a `timestamp`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.log(:info, %{track_id: 123, log: "Some info to log on Kafka"}, metadata: %{})
+  :ok
+  ```
+  """
+  def log(level, log) when is_map(log) do
+    log(level,
+        Map.put(log, :timestamp, DateTime.utc_now)
+    )
+  end
+
+  @doc """
+  The function receive two arguments the log level that is an `Atom` and
+  could be (`:info`, `:debug`, `:warn` or `:error`), the second argument is
+  the log that is a `String`
+  ## Example
+  ```elixir
+  iex> ExKafkaLogger.log(:info, "Some info to log on Kafka")
+  :ok
+  ```
+  """
+  def log(level, content) when is_bitstring(content) do
+    log(level, %{content: content})
+  end
+end
