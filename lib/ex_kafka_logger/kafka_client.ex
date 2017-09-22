4472716ba063e9154d4686bedbe59244c9fd11e3
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2017-09-22 15:45:31 -0300

    Remove unecessary `IO.puts`

--- lib/ex_kafka_logger/kafka_client.ex
+++ lib/ex_kafka_logger/kafka_client.ex
@@ -5,7 +5,6 @@ defmodule ExKafkaLogger.KafkaClient do
   @topic Application.get_env(:ex_kafka_logger, :kafka_topic, "logging")
 
   def produce(message) do
-    IO.puts "\nProduce Message ******"
     key = "#{0..9999 |> Enum.take_random(1) |> List.first}"
     Producer.produce_sync(@topic, [{key, message}])
     :ok
