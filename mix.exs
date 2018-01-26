96adf245c0212abf6079b9e6de08ec993827a5af
Jefferson Stachelski <jefferson.stachelski@deepx.it>
2018-01-26 12:03:21 -0200

    New release, minor version upgrade

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKafkaLogger.Mixfile do
   def project do
     [
       app: :ex_kafka_logger,
-      version: "3.0.0",
+      version: "3.1.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
